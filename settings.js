1dfc2f439f2d73db3da13fe2b93004ef57b5e4c6
constracti <constracti@gmail.com>
2017-06-05 14:24:00 +0300

    get confirm message from the element after the button

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ jQuery( document ).ready( function( $ ) {
 
 $( '.kgr-social-login-button' ).click( function() {
 	var button = $( this );
-	var msg = button.parent().siblings( '.description' ).html();
+	var msg = button.parent().next( '.description' ).html();
 	if ( !confirm( msg ) )
 		return false;
 	$.get( button.prop( 'href' ), function( data ) {
