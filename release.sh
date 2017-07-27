e666a4083e47a9b1c0b34592a3470d907c0fbf6d
constracti <constracti@gmail.com>
2017-07-27 23:43:30 +0300

    exclude composer directory from release

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -N https://curl.haxx.se/ca/cacert.pem
-zip kgr-social-login.zip -u -r composer/ images/ *.css *.js *.php cacert.pem
+zip kgr-social-login.zip -u -r images/ *.css *.js *.php cacert.pem
