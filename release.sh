0a7bd2ac714e31fd9ae503a51320b88f11653f03
constracti <constracti@gmail.com>
2017-10-02 19:02:04 +0300

    include cacert.pem

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-zip kgr-social-login.zip -u -r images/ *.css *.js *.php
+wget -N https://curl.haxx.se/ca/cacert.pem
+zip kgr-social-login.zip -u -r images/ *.css *.js *.php cacert.pem
